5639b6d921b8bb7048733c1aa339ae0c537e30f0
Matheus Siqueira <69863013+om4ths@users.noreply.github.com>
2025-06-24 13:09:39 +0000

    requerimentos

--- bot.py
+++ bot.py
@@ -303,3 +303,21 @@ async def on_ready():
     print(f"🤖 Bot conectado como {bot.user}")
 
 bot.run(TOKEN)
+
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot online!"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+def manter_online():
+    t = Thread(target=run)
+    t.start()
+
+manter_online()
